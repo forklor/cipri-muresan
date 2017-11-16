7945104f5946b5428c0d6a0cd6da6bc642b97155
Adrian Geana <geana.adrian@gmail.com>
2017-11-16 13:18:38 +0200

    Reverse keypad

--- modules/keypad.cpp
+++ modules/keypad.cpp
@@ -4,11 +4,19 @@ const byte ROWS = 4; //four rows
 const byte COLS = 4; //three columns
 
 char keys[ROWS][COLS] = {
-	{'1','2','3', 'A'},
+	/*{'1','2','3', 'A'},
 	{'4','5','6', 'B'},
 	{'7','8','9', 'C'},
-	{'*','0','#', 'D'}
+	{'*','0','#', 'D'} */
+
+	{'D', '#', '0', '*'},
+	{'C', '9', '8', '7'},
+	{'B', '6', '5', '4'},
+	{'A', '3', '2', '1'},
 };
+
+	
+
 
 byte rowPins[ROWS] = {2, 3, 4, 5}; //connect to the row pinouts of the keypad
 byte colPins[COLS] = {6, 7, 8, 9}; //connect to the column pinouts of the keypad
